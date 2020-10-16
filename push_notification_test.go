5b94d752ca3ba8b9717496dee6fd870a5cf89fac
Marcel Gregoriadis <marcel.g98@live.de>
2020-10-16 10:54:44 +0200

    improved push notification tests

--- push_notification_test.go
+++ push_notification_test.go
@@ -7,6 +7,8 @@ import (
 
 func TestSendPushNotification(t *testing.T) {
 	t.Run("send to valid user", func(t *testing.T) {
+		setup()
+
 		i, err := SendPushNotification(&PushNotificationInput{
 			Audience: PushNotificationAudienceInput{
 				Email: "john.doe@gmail.com", // a user with this email exists in the Adalo app
@@ -22,6 +24,8 @@ func TestSendPushNotification(t *testing.T) {
 	})
 
 	t.Run("send to user that does not exist", func(t *testing.T) {
+		setup()
+
 		i, err := SendPushNotification(&PushNotificationInput{
 			Audience: PushNotificationAudienceInput{
 				Email: "invalid@gmail.com",
@@ -35,5 +39,37 @@ func TestSendPushNotification(t *testing.T) {
 		assert.Error(t, err)
 		assert.Equal(t, ErrorUserNotFound, err)
 		assert.Equal(t, 0, i)
+	})
+
+	t.Run("unauthorized", func(t *testing.T) {
+		setup(unauthorized)
+
+		_, err := SendPushNotification(&PushNotificationInput{
+			Audience: PushNotificationAudienceInput{
+				Email: "john.doe@gmail.com",
+			},
+			Notification: PushNotificationContentInput{
+				Title: "Notification Title",
+				Body:  "Click this notification to learn more",
+			},
+		})
+
+		assert.Equal(t, ErrorUnauthorized, err)
+	})
+
+	t.Run("invalid app", func(t *testing.T) {
+		setup(invalidApp)
+
+		_, err := SendPushNotification(&PushNotificationInput{
+			Audience: PushNotificationAudienceInput{
+				Email: "john.doe@gmail.com",
+			},
+			Notification: PushNotificationContentInput{
+				Title: "Notification Title",
+				Body:  "Click this notification to learn more",
+			},
+		})
+
+		assert.Equal(t, ErrorAppMismatch, err)
 	})
 }
